a5c7dc173c092b162aefd28c2546d75bdfc38705
Rbach24 <96384502+Rbach24@users.noreply.github.com>
2024-10-06 16:41:41 -0400

    Add files via upload

---
+++ RecAnalytics/src/main/java/com/RecAnalytics/RecAnalytics/RecAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.RecAnalytics.RecAnalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecAnalyticsApplication.class, args);
+	}
+
+}
---
+++ RecAnalytics/src/test/java/com/RecAnalytics/RecAnalytics/RecAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RecAnalytics.RecAnalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
